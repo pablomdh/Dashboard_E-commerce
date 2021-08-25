ad851bb13115479712fa51d2cb5dc6ef52cccc47
MateoTonkovic <mateotonkovic03@gmail.com>
2021-08-25 19:22:31 -0300

    Page user

--- src/components/Users/PageUsers.jsx
+++ src/components/Users/PageUsers.jsx
@@ -26,13 +26,42 @@ function PageUsers() {
       })
       .catch((err) => console.log(err));
   });
+  const handleDestroyUser = (id) => {
+    axios
+      .delete(`http://localhost:3000/user`, {
+        data: { id },
+        headers: { Authorization: `Bearer ${accessKey.accesToken}` },
+      })
+      .then(() => {
+        const newUsers = users.filter((item) => item.id !== id);
+
+        setUsers(newUsers);
+      })
+      .catch((err) => console.log(err));
+  };
+  const handleDestroyAdmin = (id) => {
+    axios
+      .delete(`http://localhost:3000/admin`, {
+        data: { id },
+        headers: { Authorization: `Bearer ${accessKey.accesToken}` },
+      })
+      .then(() => {
+        const newUsers = users.filter((item) => item.id !== id);
+
+        setUsers(newUsers);
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="container">
-      <h1>Estos son los admins!</h1>
-      <table>
+      <h2>Estos son los admins!</h2>
+      <table className="table">
         <thead>
-          <td>Name</td>
-          <td>Email</td>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Actions</th>
+          </tr>
         </thead>
         <tbody>
           {admins.map((admin) => (
@@ -46,22 +75,40 @@ function PageUsers() {
                 <span>{admin.email}</span>
               </td>
               <td>
-                <span>{admin.addresses}</span>
-              </td>
-              <td>
-                <span>{admin.phonenumbers}</span>
+                <div className="d-flex justify-content-between">
+                  <span
+                    onClick={() => {
+                      handleDestroyAdmin(admin.id);
+                    }}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="25"
+                      height="25"
+                      fill="currentColor"
+                      className="bi bi-x-circle"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                      <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                    </svg>
+                  </span>
+                </div>
               </td>
             </tr>
           ))}{" "}
         </tbody>
       </table>
       <h2>Estos son los usuarios!</h2>
-      <table>
+      <table className="table">
         <thead>
-          <td>Name</td>
-          <td>Email</td>
-          <td>Address</td>
-          <td>Phone number</td>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Address</th>
+            <th scope="col">Phone number</th>
+            <th scope="col">Actions</th>
+          </tr>
         </thead>
         <tbody>
           {users.map((user) => (
@@ -79,6 +126,27 @@ function PageUsers() {
               </td>
               <td>
                 <span>{user.phonenumbers}</span>
+              </td>
+              <td>
+                <div className="d-flex justify-content-between">
+                  <span
+                    onClick={() => {
+                      handleDestroyUser(user.id);
+                    }}
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="25"
+                      height="25"
+                      fill="currentColor"
+                      className="bi bi-x-circle"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                      <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                    </svg>
+                  </span>
+                </div>
               </td>
             </tr>
           ))}
