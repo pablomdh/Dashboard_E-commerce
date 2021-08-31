e38bcacbcc32e6c1bdce2d9f8f00227cbcd5001a
maurogoyeneche <goyenechemauro@gmail.com>
2021-08-31 17:59:53 -0300

    sidebar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -2,16 +2,31 @@ import styles from "./SideBar.module.css";
 
 const SideBar = () => {
   return (
-    <div>
+    <>
       <div className={`${styles.sideBar}`}>
-        <a className="active" href="#home">
-          Home
+        <a className="navbar-brand" href="...">
+          LOGO
         </a>
-        <a href="#news">News</a>
-        <a href="#contact">Contact</a>
-        <a href="#about">About</a>
+        <div className="d-flex  flex-column justify-content-around h-100">
+          <div>
+            <a className="active" href="#home">
+              Inicio
+            </a>
+            <a href="#news">Productos</a>
+            <a href="#contact">+ Producto</a>
+            <a href="#contact">+ Categoria</a>
+            <a href="#contact">Usuarios</a>
+            <a href="#contact">Ordenes</a>
+            <a href="#about">Estadisticas</a>
+          </div>
+
+          <div>
+            <a href="#about">Login</a>
+            <a href="#about">Logout</a>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
