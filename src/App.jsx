ba74aeb5a807a91122d830ad3f179d16f42f920c
maurogoyeneche <goyenechemauro@gmail.com>
2021-09-01 14:11:31 -0300

    dashoboard Login Logout Links  OK

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   return (
     <div className="App d-flex flex-column h-100">
       <Router>
-        <Navbar />
+        {/* <Navbar /> */}
         <SideBar />
         <div>
           <Switch>
